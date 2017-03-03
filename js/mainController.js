9c8358f782dbda41c13a9173e00ac9af4c369762
Will Whitmey <w.whitmey@outlook.com>
2017-03-03 18:48:10 +0000

    Array of users from API logged in console, had to use Chrome extension to stop cross origin block error

--- js/mainController.js
+++ js/mainController.js
@@ -1,7 +1,19 @@
 angular.module('TalkTalkApp')
   .controller('MainController', MainController);
 
-function MainController() {
-  this.user = { name: 'TalkTalk' };
-  return this;
-}
+  MainController.$inject = ['$http'];
+
+function MainController($http) {
+
+  this.all = [];
+
+  function getUsers() {               // Get request to API for list of users
+    $http.get('http://www.mocky.io/v2/58b92a980f00008609f09be1')
+    .then(function(response) {
+      this.all = response.data;
+      console.log(this.all);
+    });
+  }
+  getUsers();
+
+};
